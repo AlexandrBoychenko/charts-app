5fce11ad9ffeded1ae6db235d252559305bb4471
AlexandrBoychenko <alex19wave>
2019-02-25 15:27:00 +0200

    New component has been added

---
+++ src/components/Charts.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Charts extends Component {
+    render() {
+        return (
+            <div className="Charts">
+                <header className="Charts-header">
+                    <p>
+                        Charts for retail
+                    </p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Charts;
