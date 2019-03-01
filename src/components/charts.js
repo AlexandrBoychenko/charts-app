d86b7e3f34adb10c8a5a3c1143d54ab4275af9f7
AlexandrBoychenko <alex19wave>
2019-03-01 13:01:32 +0200

    Styles for the correct display of the charts have been added

--- src/components/charts.js
+++ src/components/charts.js
@@ -11,59 +11,57 @@ class ChartLine extends Component {
     }
 
     componentDidUpdate() {
-        let chart = dc.lineChart('#line-chart');
-        let chart2 = dc.pieChart('#pie-chart');
+        let chartLine = dc.lineChart('#line-chart');
+        let chartPie = dc.pieChart('#pie-chart');
 
         let parameter = !!this.props.parameter ? this.props.parameter : 'markdown';
 
-        let ndx                 = crossfilter(this.props.csvData),
-            runDimension        = ndx.dimension(function(d) {return +d.week_ref;}),
-            speedSumGroup       = runDimension.group().reduceSum(function(d) {return d[parameter];}),
-            runDimension2        = ndx.dimension(function(d) {return d.item_category;}),
-            markdownSumGroup2    = runDimension2.group().reduceSum(function(d) {return d[parameter];});
+        let ndx                         = crossfilter(this.props.csvData),
+            runDimensionLinear          = ndx.dimension(function(d) {return +d.week_ref;}),
+            sumGroupLinear              = runDimensionLinear.group().reduceSum(function(d) {return d[parameter];}),
+            runDimensionPie             = ndx.dimension(function(d) {return d.item_category;}),
+            SumGroupPie                 = runDimensionPie.group().reduceSum(function(d) {return d[parameter];});
 
-        chart2.width((element) => Helpers.calcWidth(element, chart2));
+        chartLine.width((element) => Helpers.calcWidth(element, chartPie));
 
-        chart2
+        chartPie
             .height(480)
             .innerRadius(100)
-            .dimension(runDimension2)
-            .group(markdownSumGroup2)
-            .minAngleForLabel(15)
+            .dimension(runDimensionPie)
+            .group(SumGroupPie)
             .drawPaths(true)
             .legend(dc.legend())
             // workaround for #703: not enough data is accessible through .label() to display percentages
-            .on('pretransition', function(chart) {
-                chart.selectAll('text.pie-slice').text(function(d) {
-                    let resultAngle = (d.endAngle - d.startAngle) / (2*Math.PI) * 100;
+            .on('pretransition', function(chartLine) {
+                chartPie.selectAll('text.pie-slice').text(function(d) {
+                    let resultAngle = (d.endAngle - d.startAngle) / (2 * Math.PI) * 100;
                     if (resultAngle <= 3 ) {return}
                     return dc.utils.printSingleValue(resultAngle) + '%';
                 })
             });
 
-        let xAxisRange = this.setAxisRange(runDimension, 'week_ref');
+        let xAxisRange = this.setAxisRange(runDimensionLinear, 'week_ref');
 
-        chart.width((element) => Helpers.calcWidth(element, chart));
+        chartLine.width((element) => Helpers.calcWidth(element, chartLine));
 
-        chart
+        chartLine
             .height(480)
             .x(d3.scaleLinear().domain([xAxisRange.runMin, xAxisRange.runMax]))
-            .margins({top: 10, right: 70, bottom: 50, left: 100})
+            .margins({top: 10, right: 10, bottom: 40, left: 50})
             .xAxisLabel('Time')
-            .yAxisLabel(`${parameter} Sum`)
-            .renderArea(true)
+            .yAxisLabel(`${parameter[0].toUpperCase() + parameter.slice(1)} Sum`)
+            //.renderArea(true)
             .renderDataPoints(true)
-            .linearColors(["#4575b4", "#ffffbf", "#a50026"])
             .clipPadding(10)
-            .dimension(runDimension)
-            .group(speedSumGroup);
+            .dimension(runDimensionLinear)
+            .group(sumGroupLinear);
 
         dc.renderAll();
     }
 
-    setAxisRange(runDimension, key) {
-        let runMin = +runDimension.bottom(1)[0][key];
-        let runMax = +runDimension.top(1)[0][key];
+    setAxisRange(runDimensionLinear, key) {
+        let runMin = +runDimensionLinear.bottom(1)[0][key];
+        let runMax = +runDimensionLinear.top(1)[0][key];
         return {runMin, runMax}
     }
 
