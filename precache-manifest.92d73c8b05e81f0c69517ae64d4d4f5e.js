4304706e7d6dafc810d3f3bb0417657d886c5c75
AlexandrBoychenko <alex19wave>
2019-03-02 21:54:14 +0200

    Updates

---
+++ precache-manifest.92d73c8b05e81f0c69517ae64d4d4f5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7130d9b0ab6a39305190",
+    "url": "/charts-app/static/js/runtime~main.7130d9b0.js"
+  },
+  {
+    "revision": "85237d43f6ab52502bfc",
+    "url": "/charts-app/static/js/main.85237d43.chunk.js"
+  },
+  {
+    "revision": "0da440b1e891d0dc0cef",
+    "url": "/charts-app/static/js/2.0da440b1.chunk.js"
+  },
+  {
+    "revision": "85237d43f6ab52502bfc",
+    "url": "/charts-app/static/css/main.27001ab8.chunk.css"
+  },
+  {
+    "revision": "f692a26cda3a37dcca279882140a28c1",
+    "url": "/charts-app/index.html"
+  }
+];
